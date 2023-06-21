399065ced30cad234cbfe4527859506b476b8fe9
Ryan Gang <ryan-gg@outlook.com>
2023-06-21 22:08:00 +0530

    Add dataclasses for media entities.

---
+++ lastfm-to-sqlite/dataclass.py
@@ -0,0 +1,61 @@
+from typing import Optional, TypedDict
+
+
+class Tag(TypedDict):
+    name: str
+    url: str
+
+
+class Stats(TypedDict):
+    listeners: str
+    playcount: str
+
+
+class BareArtist(TypedDict):
+    name: str
+    url: str
+    image: list[dict[str, str]]
+    mbid: Optional[str]
+
+
+class Artist(BareArtist):
+    streamable: str  # ?
+    ontour: str  # Not required
+    similar: dict[str, list[BareArtist]]
+    stats: Stats
+    tags: list[Tag]
+    bio: dict[str, str]
+
+
+class ArtistRow(TypedDict):
+    name: str
+    url: str
+    mbid: Optional[str]
+    bio: str
+
+
+class BareTrack(TypedDict):
+    duration: int  # seconds
+    url: str
+    name: str
+    artist: BareArtist
+
+
+class BareAlbum(TypedDict):
+    artist: str
+    title: str
+    url: str
+    image: list[dict[str, str]]
+    mbid: Optional[str]
+
+
+class Track(BareTrack, Stats):
+    album: BareAlbum
+    toptags: dict[str, list[Tag]]
+    wiki: dict[str, str]
+
+
+class Album(BareAlbum, Stats):
+    tags: dict[str, list[Tag]]
+    tracks: dict[str, list[BareTrack]]
+    wiki: dict[str, str]
