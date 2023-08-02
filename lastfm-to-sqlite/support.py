d3160d98697197d2128932b9c7b655237621b5d2
Ryan Gang <ryan-gg@outlook.com>
2023-08-03 01:34:27 +0530

    Add `search_on_db` method.

--- lastfm-to-sqlite/support.py
+++ lastfm-to-sqlite/support.py
@@ -1,4 +1,5 @@
 from typing import Any, Optional
+from sqlite_utils import Database
 
 
 def dict_fetch(data: dict[Any, Any], *args: str) -> Any:
@@ -16,3 +17,20 @@ def dict_fetch(data: dict[Any, Any], *args: str) -> Any:
 def valid(parameter: Optional[str]) -> bool:
     # Check that a parameter is valid.
     return (parameter is not None) and (parameter != "") and (parameter != "None")
+
+
+def search_on_db(db: Database, table: str, column: str, value: str) -> Optional[str]:
+    query = f"select column from {table} where {column} = ?"
+    cursor = db.execute(query, [value])
+    results = cursor.fetchall()
+    cursor.close()
+
+    if results:
+        if len(results) > 1:
+            # TODO Possible issue ?
+            print(
+                f"Multiple results found with the same value : {table}, {column}, {value}"
+            )
+        return results[0][0]
+    else:
+        return None
