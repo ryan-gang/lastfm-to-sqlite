0625c7257493fd26223ef8bfe5b2027d48d16001
Ryan Gang <ryan-gg@outlook.com>
2023-08-04 13:11:52 +0530

    Minor fixes.

--- lastfm-to-sqlite/support.py
+++ lastfm-to-sqlite/support.py
@@ -2,24 +2,28 @@ from typing import Any, Optional
 
 
 def dict_fetch(data: dict[Any, Any], *args: str) -> Any:
-    # Safely access elements from a json object `data.
-    # args are multiple nested keys, if key doesn't exist "" is returned.
+    # Safely access elements from json object `data.
+    # Args are multiple nested keys, if key doesn't exist "" is returned.
     try:
         for arg in args:
             data = data[arg]
-    except KeyError:
+    except (KeyError, TypeError):
+        # KeyError when arg not in data.
+        # TypeError, when data is a string, and arg is not an int.
         data = ""
 
     return data
 
 
-def valid(parameter: Optional[str]) -> bool:
+def valid(parameter: Optional[Any]) -> bool:
     # Check that a parameter is valid.
     return (parameter is not None) and (parameter != "") and (parameter != "None")
 
 
 def valid_response(response: Any) -> bool:
     # Check that a parameter is valid.
+    if response is None:
+        return False
     return "error" not in response
 
 
